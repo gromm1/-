55e261a7cd3e45b6709a9d4bc1dc8b08288ffd28
gromm1 <166762961+gromm1@users.noreply.github.com>
2024-04-16 21:25:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+name = ('это моя строка')
+print (name[0]) # №1
+print (name[-1]) # №2
+print (name[2:5]) # №3
+print (name[::-1]) # №4
+print (len (name)) # №5
+print ('это новая строка' + name) # №6
